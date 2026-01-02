ea400cbe60b1e8eb0ff8a874be65420ddf94e024
NUTAD <86157431+weiweiwei8878dayo@users.noreply.github.com>
2026-01-03 02:01:22 +0900

    Update get-ids.js

--- functions/get-ids.js
+++ functions/get-ids.js
@@ -1,25 +1,23 @@
 const { getStore } = require("@netlify/blobs");
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
     const headers = {
         "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json"
     };
 
-    const pw = event.queryStringParameters.pw;
-    if (pw !== "yudai2011") {
+    if (event.queryStringParameters.pw !== "yudai2011") {
         return { statusCode: 403, headers, body: JSON.stringify({ error: "Forbidden" }) };
     }
 
     try {
-        const store = getStore("customer_ids");
+        const store = getStore({
+            name: "customer_ids",
+            siteID: process.env.SITE_ID,
+            token: process.env.NETLIFY_AUTH_TOKEN
+        });
         const list = await store.get("list", { type: "json" });
-
-        return {
-            statusCode: 200,
-            headers,
-            body: JSON.stringify(list || [])
-        };
+        return { statusCode: 200, headers, body: JSON.stringify(list || []) };
     } catch (e) {
         return { statusCode: 200, headers, body: JSON.stringify([]) };
     }
